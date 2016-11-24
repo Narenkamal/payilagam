2d60b57ed5d2a49fb08581ee1cde311dd3f2d745
Narenkamal <naren94kamal@gmail.com>
2016-11-24 07:57:50 -0800

    Add files via upload

---
+++ Sin.java
@@ -0,0 +1,16 @@
+package com.payilagam.math;
+
+public class Sin {
+	public static void main(String[] args){
+		System.out.println("value of sin is"+Math.sin(90));
+		System.out.println("value of sin is"+Math.sin(45));
+		System.out.println("value of sin is"+Math.sin(30));
+		System.out.println("value of cos is"+Math.cos(90));
+		System.out.println("value of cos is"+Math.cos(45));
+		System.out.println("value of cos is"+Math.cos(30));
+		System.out.println("value of tan is"+Math.tan(90));
+		System.out.println("value of tan is"+Math.tan(45));
+		System.out.println("value of tan is"+Math.tan(30));
+	}
+
+}
