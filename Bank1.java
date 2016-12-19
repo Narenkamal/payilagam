4a88e5cfa31847ab9d0b402ba8e116b2cb80f098
Narenkamal <naren94kamal@gmail.com>
2016-12-19 09:50:17 -0800

    Add files via upload

---
+++ Bank1.java
@@ -0,0 +1,23 @@
+package com.payilagam.java;
+
+public class Bank1 {
+	private int minbalance=500;
+	protected int getminbalance() {
+		return minbalance;
+		
+	}
+	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+
+
---
+++ Customer1.java
@@ -0,0 +1,15 @@
+package com.payilagam.java;
+
+public class Customer1 {
+
+	public static void main(String[] args) {
+		Bank1 b=new Bank1();
+       b.getminbalance();
+       
+    int a=b.getminbalance();
+    System.out.println("minbalance="+a);
+	
+
+	}
+
+}
