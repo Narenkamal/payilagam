5341dbe9fb4f23e31af65f4f09146b9f1d2fac2c
Narenkamal <naren94kamal@gmail.com>
2016-12-24 05:25:02 -0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -4,32 +4,37 @@ class vehicle {
 	String color;
 	int speed;
 	int gear;
+	int model;
+	int cc;
 	public void attributes(){
-		System.out.println("color"+color);
-		System.out.println("speed"+speed);
-		System.out.println("gear"+gear);
+		System.out.println("color="+color);
+		System.out.println("speed="+speed);
+		System.out.println("gear="+gear);
+		System.out.println("model="+model);
+		System.out.println("cc="+cc);
 	}
 }
-	 class Bike extends vehicle {
-		int cc;
-		int model;
+	 class bike extends vehicle {
+		
 		public void attributesbike(){
-			System.out.println("color="+color);
-			System.out.println("speed="+speed);
-			System.out.println("gear="+gear);
-			System.out.println("model="+model);
-			System.out.println("cc="+cc);
+			
 			}
 	}
+	 class Car extends bike{
+		 public void attributecar(){
+			    }
+	 }
 	 public class Test{
 		 public static void main(String[] args){
-			 Bike b=new Bike();
+			 Car  b=new Car ();
 			 b.cc=100;
 			 b.speed=160;
 			 b.color="blue";
 			 b.gear=5;
 			 b.model=4356;
-			 b.attributesbike();
+			 b.attributecar();
+			b.attributesbike();
+			b.attributes();
 			 
 		 }
 	 }
